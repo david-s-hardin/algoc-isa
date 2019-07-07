b3ed50df1e7c7dc89650f9b9f05a05e904f32f7d
david-s-hardin <david.s.hardin@gmail.com>
2019-07-07 13:28:52 -0500

    Add files via upload
    Initial commit

---
+++ leg64.h
@@ -0,0 +1,68 @@
+// Copyright (C) 2019, David S. Hardin
+// License: A 3-clause BSD license.  See the LICENSE file in this directory.
+
+// LEG64 ISA
+
+#include <iostream>
+#include <ac_int.h>
+#include <rac.h>
+using namespace std;
+
+typedef unsigned long uint64; // Just used to facilitate printing
+
+// RAC begin
+
+typedef ac_int<1,false> ui1;
+typedef ac_int<8,false> ui8;
+typedef ac_int<10,false> ui10;
+typedef ac_int<12,false> ui12;
+typedef ac_int<32,false> ui32;
+typedef ac_int<64,false> ui64;
+
+//#define BIG_STRUCT
+
+#ifdef BIG_STRUCT
+#define RTYP void
+#define RVAL 
+#define amp(x) &x
+#define RASN
+#else
+#define RTYP BSTObj
+#define RVAL Obj
+#define amp(x) x
+#define RASN Obj =
+#endif
+
+enum leg64regType {PC, SP, REG, DMEM, CMEM};
+
+//             0    1     2  3    4    5    6    7    8     9      10   11   12
+enum leg64ops {BAD, ADD, ADDI, B, BEQ, BHI, BLS, BNE, CMP, CMPI, CONST, MUL, NOP, 
+//             13   14    15
+               SUB, SUBI, HALT, LDR, STR};
+
+
+#define REG_SZ 256
+
+#define DMEM_SZ 4096
+#define CMEM_SZ 1024
+
+// leg64 machine state
+
+struct leg64St {
+  ui10 pc;
+  ui12 sp;
+  array<ui64, REG_SZ>regs;
+  array<ui64, DMEM_SZ>dmem;
+  array<ui32, CMEM_SZ>cmem;
+  // struct leg64inst ins;    // current decoded instruction
+  ui8 opcode;               // current decoded instruction
+  ui8 op1;
+  ui8 op2;
+  ui8 op3;
+  ui1 C;                    // Carry Status Flag
+  ui1 N;                    // Negative Status Flag
+  ui1 Z;                    // Zero Status Flag
+  ui1 V;                    // oVerflow Status Flag
+  // ui64 insCount;
+  //  ui8 err;                  // leg64 machine error code
+};
